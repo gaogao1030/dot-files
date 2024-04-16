263518193a0d549c4ed0e16ede33c3ff6fb4b639
ガオガオ <gaogao1130@gmail.com>
2024-04-17 01:15:26 +0800

    update

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,23 +1,45 @@
 -- https://pycodestyle.pycqa.org/en/latest/intro.html#error-codes
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#pylsp
+
 return {
   {
     "neovim/nvim-lspconfig",
+    event = "LazyFile",
+    dependencies = {
+      { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
+      { "folke/neodev.nvim", opts = {} },
+      "mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
     ---@class PluginLspOpts
     opts = {
       ---@type lspconfig.options
-      require("lspconfig").pylsp.setup({
-        settings = {
-          pylsp = {
-            plugins = {
-              pycodestyle = {
-                -- ignore = { "W391" },
-                maxLineLength = 100,
+      servers = {
+        pylsp = {
+          settings = {
+            pylsp = {
+              plugins = {
+                pycodestyle = {
+                  ignore = { "W391" },
+                  maxLineLength = 100,
+                },
               },
             },
           },
         },
-      }),
+      },
+      -- require("lspconfig").pylsp.setup({
+      --   settings = {
+      --     pylsp = {
+      --       plugins = {
+      --         pycodestyle = {
+      --           -- ignore = { "W391" },
+      --           maxLineLength = 100,
+      --         },
+      --       },
+      --     },
+      --   },
+      -- }),
     },
   },
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,24 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    keys = {
-      { "v", desc = "Increment selection" },
-      { "V", desc = "Decrement selection", mode = "x" },
-    },
-    opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
-        "ruby",
-      })
-      opts.incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "v",
-          node_incremental = "v",
-          scope_incremental = false,
-          node_decremental = "V",
-        },
-      }
-    end,
-  },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   keys = {
+  --     { "v", desc = "Increment selection" },
+  --     { "V", desc = "Decrement selection", mode = "x" },
+  --   },
+  --   opts = function(_, opts)
+  --     -- add tsx and treesitter
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "ruby",
+  --     })
+  --     opts.incremental_selection = {
+  --       enable = true,
+  --       keymaps = {
+  --         init_selection = "v",
+  --         node_incremental = "v",
+  --         scope_incremental = false,
+  --         node_decremental = "V",
+  --       },
+  --     }
+  --   end,
+  -- },
 }
